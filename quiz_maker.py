c4a060b73cc4f74bfafa1e15dbd60a4390043312
Vince <vince.maano1@gmail.com>
2025-04-11 11:08:00 +0800

    added the first part of the while loop which is for the inputting of question and the function to stop
    I realized that the while loop is actually for the whole thing not just for the questions so I changed the pseudocode
    
    also I added comments just so its easier to follow what I'm thinking (I'll delete it next commit)

--- quiz_maker.py
+++ quiz_maker.py
@@ -2,13 +2,18 @@
 def createQuiz(filename):
   #open a file in append mode to avoid overwriting previous data
   with open(filename, "a") as f:
-    #while loop for inputting the questions
+    choices = ["a", "b", "c", "d"] #added the choices that will be used for the while loop
+    #while loop for inputting the questions and answers
+    while True:
+      question = input('Enter a question (type "stop" to finish): ')
+      if question.lower() == "stop": #lowers the input just to make sure it matches the required input I placed
+        break #it breaks as it is for the stop function
+        
+      #empty dictionary to store the answers effecrively
 
-    #empty dictionary to store the answers effecrively
+      #for loop for inputting the answers
 
-    #for loop for inputting the answers
-
-    #organizes the format of the txt
+      #organizes the format of the txt
 
 #run
 createQuiz("test.txt")
