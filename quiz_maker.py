5e8aff5d8b6e02dcf5c1092111ac34aceec485dd
Vince <vince.maano1@gmail.com>
2025-04-11 11:03:17 +0800

    added the function that will open the file in append mode
    I think this is worthy of a commit as I didn't even know this kind of thing exists and now I just learned about it

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,14 +1,14 @@
 #make a function
 def createQuiz(filename):
   #open a file in append mode to avoid overwriting previous data
+  with open(filename, "a") as f:
+    #while loop for inputting the questions
 
-  #while loop for inputting the questions
+    #empty dictionary to store the answers effecrively
 
-  #empty dictionary to store the answers effecrively
+    #for loop for inputting the answers
 
-  #for loop for inputting the answers
-
-  #organizes the format of the txt
+    #organizes the format of the txt
 
 #run
 createQuiz("test.txt")
