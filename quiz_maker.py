4971d6e1ed21deeadc77ae18a092e5b2bccb6579
Vince <vince.maano1@gmail.com>
2025-04-11 10:59:51 +0800

    Create quiz_maker.py and added the pseudocode

---
+++ quiz_maker.py
@@ -0,0 +1,14 @@
+#make a function
+def createQuiz(filename):
+  #open a file in append mode to avoid overwriting previous data
+
+  #while loop for inputting the questions
+
+  #empty dictionary to store the answers effecrively
+
+  #for loop for inputting the answers
+
+  #organizes the format of the txt
+
+#run
+createQuiz("test.txt")
