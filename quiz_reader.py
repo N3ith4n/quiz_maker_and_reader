058defb2dd88dd6e16ac5f691c8866c62630381a
Vince <vince.maano1@gmail.com>
2025-05-07 11:09:50 +0800

    Added the spec_print and animated_cented functions and the imports
    I decided to add them all in one go as it is just a copy of the functions from quiz_maker.py and I have already explained how it works in my commits in quiz_maker.py so I thought it's fine to just copy and paste it

--- quiz_reader.py
+++ quiz_reader.py
@@ -1,8 +1,59 @@
 #imports
+import os
+import time
+from math import floor
+import random
 
-#spec_print function
+#make a function that adds a delay before printing each character
+def spec_print(text, speed=0.03, new_list=False):
+	for character in text:
+		print(character, end="", flush=True)
+		time.sleep(speed)
+	if(new_list):
+ 		print("") 
 
-#animated_center function
+#make a function that produces a loading animation at the start
+def animated_center(text):
+	#functions that will get the terminal size and setup the necessary animation variables
+	center = os.get_terminal_size().columns
+	arrows = 1
+	duration = 6
+	max_arrows = 3
+
+	#another terminal clearer
+	print("\033[H\033[J", end="")
+
+	#main animation loop (while loop)
+	while duration > 0:
+		space = floor(center / 2) - (floor(len(text) / 2) + 1 + max_arrows)
+		spacer = " " * space
+
+		#functions for the animation of arrows going to the center (appearing)
+		print(spacer + f"{('>' * arrows) + ' ' * (max_arrows - arrows)} {text} {' ' * (max_arrows - arrows) + ('<' * arrows)}" + spacer, end="\033[H", flush=True)
+		time.sleep(0.1)
+		duration -= 0.1
+		arrows += 1
+
+		#if loop for the animation that makes the arrows disappear
+		if arrows > max_arrows:
+			spaces_that_eats_the_arrows = 0
+			arrows = max_arrows
+
+			time.sleep(0.3)
+			duration -= 0.3
+			
+			while arrows != 0: 
+				spaces_that_eats_the_arrows += 1
+				arrows -= 1
+				print(spacer + f"{(' ' * spaces_that_eats_the_arrows) + '>' * arrows} {text} {'<' * arrows + ' ' * spaces_that_eats_the_arrows}" + spacer, end="\033[H", flush=True)
+				time.sleep(0.1)
+				duration -= 0.1
+
+			time.sleep(0.3)
+			duration -= 0.3
+			
+	#clears the terminal once the animation has finished
+	print("\033[H\033[J", end="")
 
 #def function that will read the txt file then store it in the program to be used for the program that will run the quiz
 
