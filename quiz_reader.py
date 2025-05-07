bbb32ad637340913da848e45a573872964866968
Vince <vince.maano1@gmail.com>
2025-05-07 10:59:57 +0800

    Create quiz_reader.py and added the pseudocode
    well it's kinda late but hey we're starting now

---
+++ quiz_reader.py
@@ -0,0 +1,11 @@
+#imports
+
+#spec_print function
+
+#animated_center function
+
+#def function that will read the txt file then store it in the program to be used for the program that will run the quiz
+
+#def function that will run the quiz stored
+
+#run the program
